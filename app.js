794e8d2e4d459865a67fa6accf78f11c64729ddf
Lokmane Abdessalam <93524914+Lokman-Abd@users.noreply.github.com>
2024-04-22 22:45:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { JSONRPCServer } = require("json-rpc-2.0");
+
+const server = new JSONRPCServer();
+
+// First parameter is a method name.
+// Second parameter is a method itself.
+// A method takes JSON-RPC params and returns a result.
+// It can also return a promise of the result.
+server.addMethod("echo", ({ text }) => text);
+server.addMethod("add", ({ array }) => array.reduce((a, b) => a + b, 0));
+server.addMethod("mul", ({ a,b }) => {
+  return a * b
+});
+server.addMethod("log", ({ message }) => console.log(message));
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/json-rpc", (req, res) => {
+  const jsonRPCRequest = req.body;
+  // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
+  // It can also receive an array of requests, in which case it may return an array of responses.
+  // Alternatively, you can use server.receiveJSON, which takes JSON string as is (in this case req.body).
+  server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+    if (jsonRPCResponse) {
+      res.json(jsonRPCResponse);
+    } else {
+      // If response is absent, it was a JSON-RPC notification method.
+      // Respond with no content status (204).
+      res.sendStatus(204);
+    }
+  });
+});
+const PORT=3000
+app.listen(PORT,() => {
+  console.log(`Server is running on port ${PORT}`);
+});
